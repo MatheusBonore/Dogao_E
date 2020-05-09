8f5c331a7fb0568050e1bad069771233521fd48d
Matheus Henrique Conceição Bonore <matheus.bonore@gmail.com>
2020-05-09 14:56:59 -0300

    Entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,29 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app.routing.module';
 
+import {MatInputModule} from '@angular/material/input';
+
 import { appRoutes } from './app.routing';
 import { AppComponent } from './app.component';
 import { AutenticacaoComponent } from './autenticacao/autenticacao.component';
 import { EntrarComponent } from './autenticacao/entrar/entrar.component';
 import { CadastrarComponent } from './autenticacao/cadastrar/cadastrar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     AutenticacaoComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
-
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

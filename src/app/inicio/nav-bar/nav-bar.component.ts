fcdad3c6661fcb81e079f3f10cc94ea06160175c
Matheus Henrique Conceição Bonore <matheus.bonore@gmail.com>
2020-06-04 17:28:02 -0300

    Continuar

--- src/app/inicio/nav-bar/nav-bar.component.ts
+++ src/app/inicio/nav-bar/nav-bar.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface Filtro {
+  nome: string;
+  tipo: string;
+}
+
+declare type Filtros = Filtro[];
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -7,6 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   private _mostrarNavBar: boolean = true;
+
+  public filtros: Filtros = [
+    {
+      nome: 'Curto',
+      tipo: 'pelagem'
+    }, {
+      nome: 'Longo',
+      tipo: 'pelagem'
+    }, {
+      nome: 'Sólido',
+      tipo: ''
+    }, {
+      nome: 'Pelo Encaracolado',
+      tipo: ''
+    }, {
+      nome: 'Pelagem Dupla',
+      tipo: ''
+    }, {
+      nome: 'Escama',
+      tipo: ''
+    }, {
+      nome: 'Golden Retriever',
+      tipo: ''
+    }, {
+      nome: 'Beagle',
+      tipo: ''
+    }, {
+      nome: 'Pug',
+      tipo: ''
+    }, {
+      nome: 'Shih-Tzu',
+      tipo: ''
+    }, {
+      nome: '23 - 25 cm',
+      tipo: ''
+    }, {
+      nome: '30 cm',
+      tipo: ''
+    }, {
+      nome: '41 - 58 cm',
+      tipo: ''
+    }
+  ];
 
   set mostrarNavBar(mostrarNavBar: boolean) {
     this._mostrarNavBar = mostrarNavBar;

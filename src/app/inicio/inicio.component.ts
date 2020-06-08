f6e4b5ae5ec1b3bdfcb1da49e581cf106df45240
Matheus Henrique Conceição Bonore <matheus.bonore@gmail.com>
2020-06-08 19:08:21 -0300

    Categoria Segunda Parte

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface Filtro {
+  id: number;
+  nome: string;
+}
+
+declare type Filtros = Filtro[];
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
@@ -25,13 +32,47 @@ export class InicioComponent implements OnInit {
     return this._item;
   }
 
+  public filtros: Filtros = [
+    {
+      id: 1,
+      nome: 'Curto'
+    }, {
+      id: 2,
+      nome: 'Longo'
+    }, {
+      id: 3,
+      nome: 'Sólido'
+    }, {
+      id: 4,
+      nome: 'Pelo Encaracolado'
+    }, {
+      id: 5,
+      nome: 'Pelagem Dupla'
+    }, {
+      id: 6,
+      nome: 'Escama'
+    }, {
+      id: 7,
+      nome: 'Golden Retriever'
+    }, {
+      id: 8,
+      nome: 'Beagle'
+    }, {
+      id: 9,
+      nome: 'Pug'
+    }, {
+      id: 10,
+      nome: 'Shih-Tzu'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void { }
 
   public obterItem(item: string) {
     this.item = item;
-    if (this.item != '/inicio') {
+    if (this.item != '/inicio' && this.item == '/inicio/categoria') {
       this.mostrarInicio = false;
     }
   }
